f20b0a325341812f1b860cdaf1cfd7246594dd76
George Higbie <gehigbie@gmail.com>
2017-09-07 21:08:59 -0600

    Create a new class with multiple types of constructors

---
+++ src/com/georgehigbie/VipCustomer.java
@@ -0,0 +1,24 @@
+package com.georgehigbie;
+
+public class VipCustomer {
+
+    private String name;
+    private double creditLimit;
+    private String email;
+
+    public VipCustomer(){
+        this("Bubba Customer", 1000.00, "bubba@customer.com");
+        System.out.println("The empty constructor was called");
+    }
+
+    public VipCustomer(String name, double creditLimit){
+        this(name, creditLimit, "bubba@customer.com");
+    }
+
+    public VipCustomer(String name, double creditLimit, String email){
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.email = email;
+    }
+
+}
