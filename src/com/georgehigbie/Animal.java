dae49207bc61989121ed892c027d810d6e6afb7a
George Higbie <gehigbie@gmail.com>
2017-09-07 21:37:43 -0600

    Add constructor and getters for Animal class

--- src/com/georgehigbie/Animal.java
+++ src/com/georgehigbie/Animal.java
@@ -7,4 +7,32 @@ public class Animal {
     private int body;
     private int size;
     private int weigth;
+
+    public Animal(String name, int brain, int body, int size, int weigth) {
+        this.name = name;
+        this.brain = brain;
+        this.body = body;
+        this.size = size;
+        this.weigth = weigth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBrain() {
+        return brain;
+    }
+
+    public int getBody() {
+        return body;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getWeigth() {
+        return weigth;
+    }
 }
