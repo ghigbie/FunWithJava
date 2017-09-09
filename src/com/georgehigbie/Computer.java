93200da9f4c2161459b2ed54e81751818f70d24a
George Higbie <gehigbie@gmail.com>
2017-09-09 13:47:14 -0600

    Add new classes

--- src/com/georgehigbie/Computer.java
+++ src/com/georgehigbie/Computer.java
@@ -1,4 +1,28 @@
 package com.georgehigbie;
 
+import javax.management.monitor.Monitor;
+
 public class Computer {
+
+    private Case theCase;
+    private Monitor monitor;
+    private Motherboard motherboard;
+
+    public Computer(Case theCase, Monitor monitor, Motherboard motherboard) {
+        this.theCase = theCase;
+        this.monitor = monitor;
+        this.motherboard = motherboard;
+    }
+
+    public Case getTheCase() {
+        return theCase;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public Motherboard getMotherboard() {
+        return motherboard;
+    }
 }
